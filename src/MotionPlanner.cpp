cfab70afab231f1bedf5faececbdbb0a15fe74fd
Andrew Saba <asaba96@pitt.edu>
2018-06-08 17:22:32 -0400

    added new params, renamed param files to keep up with new platforms, moved planner params to a struct, fix node start-up order, misc. fixes

--- src/MotionPlanner.cpp
+++ src/MotionPlanner.cpp
@@ -24,6 +24,17 @@
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Accel.h"
 
+#include <planning_ros_utils/voxel_grid.h>
+#include <ros/ros.h>
+#include <planning_ros_msgs/VoxelMap.h>
+#include <planning_ros_utils/data_ros_utils.h>
+#include <planning_ros_utils/primitive_ros_utils.h>
+#include <motion_primitive_library/primitive/poly_solver.h>
+#include <motion_primitive_library/planner/mp_map_util.h>
+
+#include <iarc7_msgs/ObstacleArray.h>
+
+
 using geometry_msgs::Pose;
 using geometry_msgs::Twist;
 using geometry_msgs::Accel;
@@ -33,6 +44,29 @@ typedef actionlib::SimpleActionServer<iarc7_msgs::PlanAction> Server;
 enum class PlannerState { WAITING,
                          PLANNING };
 
+struct PlannerSettings
+{
+    // min and max arena limits
+    double max_arena_limits[3];
+    double min_arena_limits[3];
+
+    // speed, acceleration, jerk limits
+    double kinematic_constraints[3];
+
+    // pos, vel, accel tolerances
+    double tolerances[3];
+
+    double dt;
+    double eps;
+    double u_max;
+
+    int max_num;
+    int num;
+    int ndt;
+
+    vec_Vec3f U;
+};
+
 bool checkGoal(Pose& pose_goal, Twist& twist_goal,
                                 Accel& accel_goal,
                                 double kinematic_constraints[3],
@@ -40,7 +74,7 @@ bool checkGoal(Pose& pose_goal, Twist& twist_goal,
                                 double min_arena[3]) {
     if (pose_goal.position.x > max_arena[0]
         || pose_goal.position.y > max_arena[1]
-        || pose_goal.position.z > max_arena[2]){
+        || pose_goal.position.z > max_arena[2]) {
 
         ROS_ERROR("Goal provided to planner is above arena limits");
         return false;
@@ -48,7 +82,7 @@ bool checkGoal(Pose& pose_goal, Twist& twist_goal,
 
     if (pose_goal.position.x < min_arena[0]
         || pose_goal.position.z < min_arena[1]
-        || pose_goal.position.z < min_arena[2]){
+        || pose_goal.position.z < min_arena[2]) {
 
         ROS_ERROR("Goal provided to planner is below arena limits");
         return false;
@@ -88,44 +122,12 @@ int main(int argc, char **argv)
     // encapsulate parameters
     ros::NodeHandle private_nh ("~");
 
-    // LOAD PARAMETERS
-    double update_frequency;
-
-    // speed, acceleration, jerk limits
-    double kinematic_constraints[3];
-
-    // max x, y, z arena limits
-    double max_arena_limits[3];
-
-    // min x, y, z arena limits
-    double min_arena_limits[3];
-
-    // Update frequency retrieve
-    ROS_ASSERT(private_nh.getParam("planner_update_frequency", update_frequency));
-
-    // kinematic constraints retrieve
-    ROS_ASSERT(private_nh.getParam("max_speed", kinematic_constraints[0]));
-    ROS_ASSERT(private_nh.getParam("max_acceleration", kinematic_constraints[1]));
-    ROS_ASSERT(private_nh.getParam("max_jerk", kinematic_constraints[2]));
-
-    // arena size/limits retrieve
-    ROS_ASSERT(private_nh.getParam("/arena/max_x", max_arena_limits[0]));
-    ROS_ASSERT(private_nh.getParam("/arena/max_y", max_arena_limits[1]));
-    ROS_ASSERT(private_nh.getParam("/arena/max_z", max_arena_limits[2]));
-    
-    ROS_ASSERT(private_nh.getParam("/arena/min_x", min_arena_limits[0]));
-    ROS_ASSERT(private_nh.getParam("/arena/min_y", min_arena_limits[1]));
-    ROS_ASSERT(private_nh.getParam("/arena/min_z", min_arena_limits[2]));
-
     // Wait for a valid time in case we are using simulated time (not wall time)
     while (ros::ok() && ros::Time::now() == ros::Time(0)) {
         // wait
         ros::spinOnce();
         ros::Duration(.005).sleep();
     }
-
-    Server server(nh, "planner_request", false);
-    server.start();
 
     // Form a connection with the node monitor. If no connection can be made
     // assert because we don't know what's going on with the other nodes.
@@ -135,6 +137,65 @@ int main(int argc, char **argv)
     
     ROS_ASSERT_MSG(safety_client.formBond(),
                    "motion_planner: Could not form bond with safety client");
+
+    // start action server
+    Server server(nh, "planner_request", false);
+    server.start();
+
+    // LOAD PARAMETERS
+
+    // node update frequency
+    double update_frequency;
+
+    // Update frequency retrieve
+    ROS_ASSERT(private_nh.getParam("planner_update_frequency", update_frequency));
+
+    // settings for planner
+    PlannerSettings settings;
+
+    // max x, y, z arena limits
+    ROS_ASSERT(private_nh.getParam("/arena/max_x", settings.max_arena_limits[0]));
+    ROS_ASSERT(private_nh.getParam("/arena/max_y", settings.max_arena_limits[1]));
+    ROS_ASSERT(private_nh.getParam("/arena/max_z", settings.max_arena_limits[2]));
+
+    // min x, y, z arena limits
+    ROS_ASSERT(private_nh.getParam("/arena/min_x", settings.min_arena_limits[0]));
+    ROS_ASSERT(private_nh.getParam("/arena/min_y", settings.min_arena_limits[1]));
+    ROS_ASSERT(private_nh.getParam("/arena/min_z", settings.min_arena_limits[2]));
+
+    // speed, acceleration, jerk limits
+    ROS_ASSERT(private_nh.getParam("max_speed", settings.kinematic_constraints[0]));
+    ROS_ASSERT(private_nh.getParam("max_acceleration", settings.kinematic_constraints[1]));
+    ROS_ASSERT(private_nh.getParam("max_jerk", settings.kinematic_constraints[2]));
+
+    ROS_ASSERT(private_nh.getParam("dt", settings.dt));
+    ROS_ASSERT(private_nh.getParam("eps", settings.eps));
+    ROS_ASSERT(private_nh.getParam("ndt", settings.ndt));
+
+    ROS_ASSERT(private_nh.getParam("u_max", settings.u_max));
+    ROS_ASSERT(private_nh.getParam("max_num",settings. max_num));
+    ROS_ASSERT(private_nh.getParam("num", settings.num));
+
+    // kinematic tolerances
+    ROS_ASSERT(private_nh.getParam("p_tol", settings.tolerances[0]));
+    ROS_ASSERT(private_nh.getParam("v_tol", settings.tolerances[1]));
+    ROS_ASSERT(private_nh.getParam("a_tol", settings.tolerances[2]));
+
+    vec_Vec3f U;
+    const decimal_t du = settings.u_max / settings.num;
+    for (decimal_t dx = -settings.u_max; dx <= settings.u_max; dx += du) {
+        for (decimal_t dy = -settings.u_max; dy <= settings.u_max; dy += du) {
+            for (decimal_t dz = -settings.u_max; dz <= settings.u_max; dz += du) {
+                U.push_back(Vec3f(dx, dy, dz));
+            }
+        }
+    }
+
+    settings.U = U;
+
+    // debug publishers
+    ros::Publisher map_pub = nh.advertise<planning_ros_msgs::VoxelMap>("voxel_map", 1, true);
+    ros::Publisher refined_traj_pub = nh.advertise<planning_ros_msgs::Trajectory>("trajectory_refined", 1, true);
 
     // Cache the time
     ros::Time last_time = ros::Time::now();
@@ -190,9 +251,9 @@ int main(int argc, char **argv)
                 accel_goal = goal_->goal.motion_point.accel;
 
                 if (!checkGoal(pose_goal, twist_goal, accel_goal,
-                                        kinematic_constraints,
-                                        max_arena_limits,
-                                        min_arena_limits)) {
+                                        settings.kinematic_constraints,
+                                        settings.max_arena_limits,
+                                        settings.min_arena_limits)) {
                     ROS_ERROR("Planner aborting requested gaol");
                     server.setAborted();
                     state = PlannerState::WAITING;
@@ -212,6 +273,8 @@ int main(int argc, char **argv)
                 bool success_ = true;
                 
                 // planning calls will go here
+
+
 
                 result_.success = success_;
                 
